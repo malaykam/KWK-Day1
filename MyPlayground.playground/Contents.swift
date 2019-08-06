e2130da3c19bef6d32a1203a39f9f64e3fa5aebd
Malayka <malayka@Apples-MacBook-Pro-35.local>
2019-08-06 10:40:06 -0400

    IDK

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//Directions: Write code to perform each of the following steps.
+//1. Declare a variable named favUniversity.
+var favUniversity = "New york University"
+print("I love \(favUniversity)")
+//2. Print a statement about your variable. Be sure to use interpolation.
+//3. Change your favUniversity.
+favUniversity = "St.Johns university"
+//4. Print a statement about your new favUniversity.
+print("I love \(favUniversity) too!")
+//5. Declare a constant with the city your new favorite university is located in named favCity.
+let favCity = "New york city"
+//6. Print a statement about your constant.
+print("M")
+//7. Extension: Concatenate two strings. It can be about anything.
+//8. Print the strings using interpolation.
